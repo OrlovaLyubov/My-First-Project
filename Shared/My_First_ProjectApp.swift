6c808207859fc55de716c2281a04529102377af9
Orlova Lyubov <lyubko.orlova@yandex.ru>
2022-10-09 15:55:05 +0300

    Initial Commit

---
+++ Shared/My_First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_ProjectApp.swift
+//  Shared
+//
+//  Created by Любовь Орлова on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct My_First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
